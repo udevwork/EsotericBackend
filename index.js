79dbb8ff4cd9b42ad9dde645ce635c8df373d1b1
Denis <udevwork@gmail.com>
2024-01-10 08:21:06 +0100

    new func

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const https = require("https")
 const fs = require('fs-extra')
+const router = express.Router();
+const subdomain = require('express-subdomain');
 const port = 443
 
 require('dotenv').config()
@@ -16,18 +18,48 @@ var options = {
   cert: fs.readFileSync('./sslfiles/gpterica_space.crt')
 }
 
-https.createServer(options, app).listen(443);
+if (process.env.SCHEMA == 'DEBUG') {
+  app.listen(80, () => {
+    console.log(`Example app listening on port ${80}`)
+  })
+}
 
+if (process.env.SCHEMA == 'RELEASE') {
+  https.createServer(options, app).listen(port);
+  console.log(`App started with RELEASE schema`)
+}
 
 app.get('/ask', async (req, res) => {
   try {
     let promt = req.query.promt;
     const chatCompletion = await openai.chat.completions.create({
       messages: [{ role: 'user', content: promt }],
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-3.5-turbo'
     });
     var message = chatCompletion.choices[0].message.content
     res.json({ message: message });
+  } catch (error) {
+    res.json({ error: error });
+  }
+})
+
+app.get('/v2/ask', async (req, res) => {
+  try {
+    console.log(req.query);
+   
+    // Разбираем каждую строку JSON в объект
+    const messages = req.query.messages.map(message => JSON.parse(message));
+   
+    const chatCompletion = await openai.chat.completions.create({
+      messages: messages,
+      model: "gpt-3.5-turbo-1106",
+      response_format: { type: "json_object" },
+    });
+
+    var content = chatCompletion.choices[0].message
+    console.log(content)
+    res.json(content);
+
   } catch (error) {
     res.json({ error: error });
   }
@@ -40,11 +72,3 @@ app.get('/', async (req, res) => {
     res.json({ error: error });
   }
 })
-
-// app.get('/', (req, res) => {
-//   res.send('33BD10F8C1951C6BA5B24BDE8668A077.68D6766C7D7FD4EEDAAD669FFECA15C6.NwPsb0M6bj.comodoca.com')
-// })
-
-// app.listen(80, () => {
-//   console.log(`Example app listening on port ${80}`)
-// })
